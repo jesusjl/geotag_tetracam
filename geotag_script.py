04e61531e64f9b75d265cab75fe833060d5d7649
Jesús <gesusjl@gmail.com>
2018-02-05 08:35:19 -0500

    fix indent error

--- geotag_script.py
+++ geotag_script.py
@@ -15,15 +15,16 @@ def list_csv(input_folder, csv_file_name):
     return csv_list
 
 
+
 def geotag_file(input_folder, csv_list, ext):
-        """
-            Geotag files. Require a path to the folder containing the images, the list returned by list_csv function and the images extension.
-            Invalid tags should be removed before saving metadata to avoid errors.
-        """
+    """
+        Geotag files. Require a path to the folder containing the images, the list returned by list_csv function and the images extension.
+        Invalid tags should be removed before saving metadata to avoid errors.
+    """
     for item in list(csv_list):
         try:
             filename = item[0]
-            abs_input_file_path = os.path.join(input_folder, filename + "." +ext)
+            abs_input_file_path = os.path.join(input_folder, filename + "." + ext)
             exif = GExiv2.Metadata(abs_input_file_path)
             exif.clear_tag('Exif.Image.Compression')  # remove invalid tag
             exif.set_gps_info(float(item[2]), float(item[1]), float(item[3])) # long lat alt
